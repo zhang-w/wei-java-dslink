8bc1f0aa2ceabc1a86b9af9bf7b1fe00037dde04
zshwei@cn.ibm.com <zshwei@cn.ibm.com>
2016-03-03 01:02:17 +0800

    remove simulator

--- src/main/java/org/dsa/iot/wei/Main.java
+++ src/main/java/org/dsa/iot/wei/Main.java
@@ -1,88 +1,145 @@
 package org.dsa.iot.wei;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.dsa.iot.dslink.DSLink;
 import org.dsa.iot.dslink.DSLinkFactory;
 import org.dsa.iot.dslink.DSLinkHandler;
-import org.dsa.iot.dslink.DSLinkProvider;
-import org.dsa.iot.dslink.node.value.SubscriptionValue;
+import org.dsa.iot.dslink.node.Node;
+import org.dsa.iot.dslink.node.NodeBuilder;
+import org.dsa.iot.dslink.node.Permission;
+import org.dsa.iot.dslink.node.Writable;
+import org.dsa.iot.dslink.node.actions.Action;
+import org.dsa.iot.dslink.node.actions.ActionResult;
+import org.dsa.iot.dslink.node.actions.Parameter;
+import org.dsa.iot.dslink.node.actions.table.Row;
+import org.dsa.iot.dslink.node.actions.table.Table;
+import org.dsa.iot.dslink.node.value.Value;
+import org.dsa.iot.dslink.node.value.ValuePair;
+import org.dsa.iot.dslink.node.value.ValueType;
+import org.dsa.iot.dslink.util.Objects;
 import org.dsa.iot.dslink.util.handler.Handler;
+import org.dsa.iot.dslink.util.json.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.dsa.iot.wei.simulators.SimGatewayDevice0;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
- * The main class that starts the DSLink and list everything from root.
+ *
  */
 public class Main extends DSLinkHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-	private DSLinkProvider provider;
-  private CountDownLatch latch;
+	public static final String	NAME	 = "simGatewayDevice0";
+	private static final Logger	LOGGER = LoggerFactory.getLogger(Main.class);
+	private static final Random	RANDOM = new Random();
+
+	@Override
+	public JsonObject getLinkData() {
+		JsonObject obj = new JsonObject();
+		obj.put(NAME, true);
+		return obj;
+	}
+
+	@Override
+	public boolean isResponder() {
+		return true;
+	}
 
   @Override
   public void preInit() {
-      // Latch is used to ensure responder is initialized first
-      latch = new CountDownLatch(1);
-  }
-  
-	@Override
-	public boolean isRequester() {
-		return true;
-	}
-	
-  @Override
-  public boolean isResponder() {
-      return true;
+      //String path = getWorkingDir().getAbsolutePath();
+      //LOGGER.info("Current working directory: {}", path);
   }
 
 	@Override
-	public void onRequesterConnected(final DSLink link) {
-		LOGGER.info("--------------");
-		LOGGER.info("Connected!");
-		
-		subscribe(link);
+	public void onResponderInitialized(DSLink link) {
+		NodeBuilder builder = link.getNodeManager().createRootNode(NAME);
+		Node node = builder.build();
+
+		//initSettableNode(node);
+		initEventNode(node);
+		initActionNode(node);
+		LOGGER.info("Simulator {} initialized.", Main.NAME);
 	}
 
 	@Override
-	public void onRequesterDisconnected(DSLink link) {
-		LOGGER.info("Disconnected with broker.");
+	public void onResponderConnected(DSLink link) {
+		LOGGER.info("Responder connected");
 	}
-	
-  @Override
-  public void onResponderInitialized(DSLink link) {
-		  SimGatewayDevice0.init(link);
-      LOGGER.info("Responder initialized");
-      latch.countDown();
-  }
 
 	public static void main(String[] args) {
-		Main main = new Main();
-		main.provider = DSLinkFactory.generate(args, main);
-		if (main.provider == null) {
-			return;
-		}
-		main.provider.start();
+		DSLinkFactory.start(args, new Main());
 	}
 
-
 	/**
-	 * Subscribes to the simGatewayDevice0 node and display it from DgLux.
+	 * Initializes the 'settable' node. By default this node will have a value of
+	 * 'UNSET' until it is set by the requester.
 	 *
-	 * @param link
-	 *          Requester link used to communicate to the endpoint.
+	 * @param node
+	 *          Values node.
+	 * @see org.dsa.iot.dual.requester.Requester#setNodeValue
 	 */
-	private static void subscribe(DSLink link) {
-		String path = link.getPath() + "/" + SimGatewayDevice0.NAME + "/event";
-		LOGGER.info("Subscribing {} ...", path);
-		link.getRequester().subscribe(path, new Handler<SubscriptionValue>() {
+	private static void initSettableNode(Node node) {
+		NodeBuilder builder = node.createChild("settable");
+		builder.setWritable(Writable.WRITE);
+		builder.setValueType(ValueType.STRING);
+		builder.setValue(new Value("UNSET"));
+		builder.getListener().setValueHandler(new Handler<ValuePair>() {
 			@Override
-			public void handle(SubscriptionValue event) {
-				int val = event.getValue().getNumber().intValue();
-				LOGGER.info("Received new event: {}", val);
+			public void handle(ValuePair event) {
+				String val = event.getCurrent().getString();
+				LOGGER.info("Responder has a new value set from requester: {}", val);
+				LOGGER.info("External source? " + event.isFromExternalSource());
 			}
 		});
-		LOGGER.info("Subscribed {}.", path);
+		node = builder.build();
+		node.setSerializable(false);
+		LOGGER.info("Responder has a current value of {}", node.getValue().toString());
+	}
+
+	/**
+	 * Initializes the 'event' node which hold events from this device. This node
+	 * can be subscribed to in order to get events generated by this device.
+	 *
+	 * @param node
+	 *          Values node.
+	 * @see org.dsa.iot.dual.requester.Requester#subscribe
+	 */
+	private static void initEventNode(Node node) {
+		NodeBuilder builder = node.createChild("event");
+		final Node child = builder.build();
+		child.setValueType(ValueType.DYNAMIC);
+
+		Objects.getDaemonThreadPool().scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				double randomVoltate = RANDOM.nextDouble() * 3.3;
+				JsonObject jsObj = new JsonObject().put("voltage", randomVoltate);
+				Value val = new Value(jsObj.toString());
+				val.setSerializable(false);
+				child.setValue(val);
+				LOGGER.info("Generated new event on {}: {}", child.getPath(), val);
+			}
+		}, 0, 5, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * Initializes the 'action' node that can be invoked.
+	 *
+	 * @param node
+	 *          Values node.
+	 * @see org.dsa.iot.dual.requester.Requester#invoke
+	 */
+	private static void initActionNode(Node node) {
+		NodeBuilder builder = node.createChild("turnOff");
+		builder.setAction(new Action(Permission.READ, new Handler<ActionResult>() {
+			@Override
+			public void handle(ActionResult event) {
+				LOGGER.info("Responder action turnOff invoked from requester");
+				Table t = event.getTable();
+				t.addRow(Row.make(new Value("Turned Off")));
+			}
+		}).addResult(new Parameter("response", ValueType.STRING)));
+		builder.build();
 	}
 }
